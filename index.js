d35cbf210153cef051ef71051b6196bf8479755a
FelicityHvH <FelicityHvH@gmail.com>
2022-08-28 19:51:34 -0300

    master

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const port = process.env.PORT || 3000;
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
@@ -51,6 +53,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('listening on *: 3000');
 });
